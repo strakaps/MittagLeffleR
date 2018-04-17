ade95a39e3ea0e7c6a0e8b85e8ab03a95041e98a
Peter Straka <p.straka@unsw.edu.au>
2018-04-17 11:41:01 +0800

    initialize MLMLE with log-moment estimate

--- R/MLMLE.R
+++ R/MLMLE.R
@@ -1,17 +1,25 @@
 #' Maximum Likelihood Estimation of the Mittag-Leffler distribution
-#' 
 #'
+#' @export
+mlml <- function(data) {
+  # theta_orig: the parameter (tail, scale)
+  # theta: a transformed parameter so that optimisation is unconstrained
+  theta_orig <- function(theta)
+    c(1 / (1 + exp(-theta[1])), exp(theta[2]))
+  theta <-
+    function(theta_orig)
+      c(-log(1 / theta_orig[1] - 1), log(theta_orig[2]))
+  # find initial estimate
+  theta_orig_init <- logMomentEstimator(data)[1:2]
+  theta_init <- theta(theta_orig_init)
 
-mlml <- function(X) {
+  # run optimization in unconstrained parameters
   log_l <- function(theta) {
-    #transform parameters so can do optimization unconstrained
-    theta[1] <- 1/(1+exp(-theta[1]))
-    theta[2] <- exp(theta[2])
-    -sum(log(dml(X,theta[1],theta[2])))
+    theta <- theta_orig(theta)
+    - sum(dml(data, theta[1], theta[2], log = TRUE))
   }
-  ml_theta <- stats::optim(c(0.5,0.5), fn=log_l)$par
-  ml_a <- 1/(1 + exp(-ml_theta[1]))
+  ml_theta <- stats::optim(theta_init, fn = log_l)$par
+  ml_a <- 1 / (1 + exp(-ml_theta[1]))
   ml_d <- exp(ml_theta[2])
-  print(paste("tail =", ml_a, "scale =", ml_d))
+  c(tail = ml_a, scale = ml_d)
 }
-mlml(rml(n = 100, tail = 0.9, scale = 2))
