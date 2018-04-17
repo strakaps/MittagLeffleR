2a988cff8d6b18deea88e80d1a54034d64d35a56
Peter Straka <p.straka@unsw.edu.au>
2018-04-17 12:33:20 +0800

    fix: logistic transform excluded 1 as valid tail parameter

--- R/MLMLE.R
+++ R/MLMLE.R
@@ -1,23 +1,27 @@
 #' Maximum Likelihood Estimation of the Mittag-Leffler distribution
 #'
-#' Optimizes the bivariate likelihood function via 
-#' \code{\link{optim}}. Uses \code{\link{logMomentEstimator}}
+#' Optimizes the bivariate loglikelihood of the Mittag-Leffler distribution
+#'  via \code{\link{optim}}. Uses \code{\link{logMomentEstimator}}
 #' for initial parameter values.
 #'
 #' @param data Vector of class "numeric"
 #' @param ... Additional parameters passed on to \code{\link{optim}}. 
-#' @return A named vector of class "numeric"
+#' @return The output of \code{\link{optim}}.
+#' @examples
+#' library(magrittr)
+#' rml(n = 1000, tail = 0.8, scale = 1000) %>% mlmle()
 #' @export
-mlml <- function(data, ...) {
+mlmle <- function(data, ...) {
   # theta_orig: the parameter (tail, scale)
   # theta: a transformed parameter so that optimisation is unconstrained
   theta_orig <- function(theta)
-    c(1 / (1 + exp(-theta[1])), exp(theta[2]))
+    c(1.1 / (1 + exp(-theta[1])), exp(theta[2]))
   theta <-
     function(theta_orig)
-      c(-log(1 / theta_orig[1] - 1), log(theta_orig[2]))
+      c(-log(1.1 / theta_orig[1] - 1), log(theta_orig[2]))
   # find initial estimate
   theta_orig_init <- logMomentEstimator(data)[1:2]
+  theta_orig_init[1] <- min(theta_orig_init[1], 0.99)
   theta_init <- theta(theta_orig_init)
 
   # run optimization in unconstrained parameters
@@ -26,9 +30,6 @@ mlml <- function(data, ...) {
     - sum(dml(data, theta[1], theta[2], log = TRUE))
   }
   opt_out <- stats::optim(theta_init, fn = log_l, ...)
-  ml_theta <- opt_out$par
-  tail <- 1 / (1 + exp(-ml_theta[1]))
-  scale <- exp(ml_theta[2])
-  opt_out$par <- c(tail, scale)
+  opt_out$par <- theta_orig(opt_out$par)
   opt_out
 }
